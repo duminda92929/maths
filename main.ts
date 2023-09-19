a407811c37623e6c5037b22985f85e8884c1aab3
duminda92929 <145429969+duminda92929@users.noreply.github.com>
2023-09-19 12:40:59 +0530

    FAN

--- main.ts
+++ main.ts
@@ -1,1 +1,10 @@
-
+basic.forever(function () {
+    basic.showNumber(input.temperature())
+    if (input.temperature() <= 21) {
+        basic.showString("Cool Condition")
+        pins.digitalWritePin(DigitalPin.P0, 0)
+    } else {
+        basic.showString("Dry Condition")
+        pins.digitalWritePin(DigitalPin.P0, 1)
+    }
+})
